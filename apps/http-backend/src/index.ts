54a5e4feeeaf2cafc3ba1138bc85f8865c39b9fd
Souvick Bhowmick <souvickb30@gmail.com>
2026-09-09 22:11:19 +0530

    updated index of express

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,11 @@
-import express from "express";
+import express, { type Express } from "express";
 import cors from "cors"
-import jwt from "jsonwebtoken"
 import { restrictedRoute } from "./middlewares/auth";
 import { handleSignIn, handleSignUp } from "./controllers/user";
 import { handleRoomCreation } from "./controllers/room";
 import { getChats } from "./controllers/chat";
 
-const app = express();
+const app: Express = express();
 app.use(cors({origin: "https://calico-rouge.vercel.app", credentials: true,}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -21,4 +20,6 @@ app
 .post("/api/room/create", restrictedRoute, handleRoomCreation)
 .get("/api/chats/:roomid", getChats)
 
-app.listen(3001, () => console.log("express server syarted running..."))
+export default app;
+
+// app.listen(3001, () => console.log("express server syarted running..."))
